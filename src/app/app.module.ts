a446274f1aaafc702f1bcf2e8393da62338ab1c4
vince <vincentverbeet@gmail.com>
2019-12-13 17:36:42 +0100

    master : pushing defautl page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent} from "./component/index/index.component";
+import { IndexComponent} from './component/index/index.component';
+import { PlayersComponent } from './component/players/players.component';
+import { JobsComponent } from './component/jobs/jobs.component';
+import { PlayerJobsComponent } from './component/player-jobs/player-jobs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    PlayersComponent,
+    JobsComponent,
+    PlayerJobsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/player-jobs/player-jobs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-jobs',
+  templateUrl: './player-jobs.component.html',
+  styleUrls: ['./player-jobs.component.scss']
+})
+export class PlayerJobsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
